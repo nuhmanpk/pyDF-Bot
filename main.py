0edd8d69c2061e208fa0f04f8d62fb6ea2e65c41
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-05 19:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,9 @@ No one gonna Help You !!
 
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/PyDF/")
 # TXT_LOCATION =  os.environ.get("TXT_LOCATION", "./DOWNLOADS/txt/")
+path = './DOWNLOADS/txt/'
+
+  
 
 START_BUTTON = InlineKeyboardMarkup(
         [[
@@ -97,10 +100,12 @@ async def pdf_to_text(bot, message):
      page_no = pdf_reader.getPage(0) # pageObject
     # text_path = TXT_LOCATION + f"txt{message.chat.id}.txt"     
      await txt.edit("Extracting Text from PDF...")
-     for page in range (0,num_of_pages):
-         os.open("bughunter0.txt",os.O_RDWR & os.O_APPEND)
-         os.write("bughunter0.txt",f"{page_no.extractText()}")
-         os.close("bughunter0.txt")
+     with open('bughunter0.txt', 'w') as fp: 
+        pass
+            for page in range (0,num_of_pages):
+              os.open("bughunter0.txt",os.O_RDWR & os.O_APPEND)
+              os.write("bughunter0.txt",f"{page_no.extractText()}")
+              os.close("bughunter0.txt")
      text_path = bughunter0.txt
      await message.reply_document(text_path,caption="©@BugHunterBots")
    #  await message.reply_document(text_path1,caption="©@BugHunterBots")
