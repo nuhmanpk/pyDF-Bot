46db699d69254add0823a8a843dc997747780ad7
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-05 13:03:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,39 @@ bughunter0 = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+START_STR = """
+Hi **{}**, I'm PyDF Bot. I can Provide all Help regarding PDF file
+"""
+ABOUT = """
+**BOT:** `PYDF BOT`
+**AUTHOR :** [bughunter0](https://t.me/bughunter0)
+**SERVER :** 'Heroku'
+**LIBRARY :** 'Pyrogram'
+**SOURCE :** [BugHunterBots](https://t.me/bughunterbots)
+**LANGUAGE :** `Python 3.9`
+"""
+HELP = """
+No one gonna Help You !!
+"""
+
+
+START_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('↗ Join Here ↗', url='https://t.me/BughunterBots')
+        ],
+        [
+        InlineKeyboardButton('ABOUT',callback_data='cbabout')
+        ],
+        [
+        InlineKeyboardButton('HELP',callback_data='cbhelp')
+        ]
+        ]
+    )
 
 @bughunter0.on_message(filters.command(["start"]))
 async def start(bot, update):
+     await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTON
+    )
