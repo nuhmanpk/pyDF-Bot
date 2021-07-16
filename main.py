dd6a457c2b8d357b369965c45767ac013eb910ec
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-16 13:59:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -101,19 +101,19 @@ async def pdf_to_text(bot, message):
           num_of_pages = pdf_reader.getNumPages()
           await txt.edit(f"Found {num_of_pages} Page")
           page_no = pdf_reader.getPage(0) # pageObject
-        # text_path = TXT_LOCATION + f"txt{message.chat.id}.txt"     
+       #   text_path = TXT_LOCATION + f"txt{message.chat.id}.txt"     
           await txt.edit("Extracting Text from PDF...")
        #   print(page_no.extractText())
        #   time.sleep(10)
           page_content = """ """ # EmptyString
           text_path = path
-      #    await txt.edit(f"This is what i found\n{page_content}")
+       #   await txt.edit(f"This is what i found \n {page_content}")
           with open('bughunter0.txt', 'w') as fp:   
                 for page in range (0,num_of_pages):
                     os.open('bughunter0.txt',os.O_RDWR & os.O_APPEND)
-                    page_content = int(float(page_no.extractText()))
-                    await message.reply_text(f"{page_content} This is what i Found in Page {page}")
-                    os.write('bughunter0.txt',page_content)
+                    page_content = str(page_no.extractText())
+                    await message.reply_text(f"{page_content} \n This is what i Found in Page {page}")
+               #     os.write('bughunter0.txt',page_content)
                     os.close('bughunter0.txt')
        
           await message.reply_document(text_path,caption="©@BugHunterBots")
