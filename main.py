365d81ece2ccdf20240a4102eee8d483902e18eb
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-29 16:03:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def pdf_to_text(bot, message):
                 pdf_path = DOWNLOAD_LOCATION + f"{message.chat.id}.pdf" #pdfFileObject
                 txt = await message.reply_text("Downloading.....")  
                 c_time = time.time()
-                await message.reply_to_message.download(pdf_path,progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```",  txt, c_time   ))  
+                await message.reply_to_message.download(pdf_path,progress=progress_for_pyrogram,progress_args=( "```Trying To Uploading```", c_time   ))  
                 await txt.edit("Downloaded File")
                 pdf = open(pdf_path,'rb')
                 pdf_reader = PyPDF2.PdfFileReader(pdf) #pdfReaderObject
