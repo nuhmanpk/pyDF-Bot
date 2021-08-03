444c272eecb34de695834cda050120dc7c614239
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-08-03 15:36:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/PyDF/")
 # TXT_LOCATION =  os.environ.get("TXT_LOCATION", "./DOWNLOADS/txt/")
 path = './DOWNLOADS/txt/bughunter0.txt'
 
-  
+Disclaimer = """ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE """  
 
 START_BUTTON = InlineKeyboardMarkup(
         [[
@@ -101,13 +101,12 @@ async def pdf_to_text(bot, message):
            if message.reply_to_message:
                 pdf_path = DOWNLOAD_LOCATION + f"{message.chat.id}.pdf" #pdfFileObject
                 txt = await message.reply("Downloading.....")
-                c_time = time.time()
                 await message.reply_to_message.download(pdf_path)  
                 await txt.edit("Downloaded File")
                 pdf = open(pdf_path,'rb')
                 pdf_reader = PyPDF2.PdfFileReader(pdf) #pdfReaderObject
                 await txt.edit("Getting Number of Pages....")
-                num_of_pages = pdf_reader.getNumPages()
+                num_of_pages = pdf_reader.getNumPages() # Number of Pages
                 await txt.edit(f"Found {num_of_pages} Page")
                 page_no = pdf_reader.getPage(0) # pageObject
                 await txt.edit("Extracting Text from PDF...")
@@ -117,13 +116,13 @@ async def pdf_to_text(bot, message):
                       file_write = open(f'{message.chat.id}.txt','a+') 
                       page_no = pdf_reader.getPage(page) # Iteration of page number
                       page_content = page_no.extractText()
-                      file_write.write(f"\n page number - {page} \n")
-                      file_write.write(f" {page_content} ")   
-                      file_write.write(f"\n © BugHunterBots \n ")
+                      file_write.write(f"\n page number - {page} \n") # writing Page Number as Title
+                      file_write.write(f" {page_content} ")   # writing page content
+                      file_write.write(f"\n © BugHunterBots \n ") # Adding Page footer
                    #  await message.reply_text(f"**Page Number  :  {page}  **\n\n  ` {page_content} `\n     @BugHunterBots\n\n") # Use this Line of code to get Pdf Text as Messages
                         
                 with open(f'{message.chat.id}.txt', 'a+') as text_path:  
-                      file_write.write("THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE")
+                      file_write.write(f"{Disclaimer}") # Disclaimer note
                       await message.reply_document(f"{message.chat.id}.txt",caption="©@BugHunterBots")      
          
                 os.remove(pdf_path)
